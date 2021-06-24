d32aef9b1e91387f1d6796dee625b5e904f89bd5
unknown <conlocxoaythuytrieu@gmail.com>
2021-06-25 02:38:48 +0700

    header to examschedule at homepage

--- module/homepage/index.php
+++ module/homepage/index.php
@@ -1,15 +1,2 @@
 <?php
-if (empty(@$_SESSION['loginusercode'])) {
-    header('Location: ' . HTTP_SERVER . 'logout.php');
-}
-$session = @$_SESSION;
-$gbeLayout = "layout.tpl";
-$gbeTemplate = "index.tpl";
-
-addGlobalJS("./assets/js/examSchedule.js");
-$params = array(
-    'type' => "today"
-);
-$jsontext = callAPI("", "classroom_getSchedule.api", array(), $params);
-$data = json_decode($jsontext, true);
-$listSchedule = $data["data"];
+header("Location:" .HTTP_SERVER. "exam_schedule/exam_schedule.gbe");
